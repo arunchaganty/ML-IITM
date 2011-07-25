52cd3a83b9c1d2c434504132dea7329fb01a5cdd
Arun Tejasvi Chaganty <arunchaganty@gmail.com>
2011-07-25 12:07:41 +0530

    Initial commit

---
+++ home/context_processors.py
@@ -0,0 +1,20 @@
+"""
+A set of request processors that return dictionaries to be merged into a
+template context. Each function takes the request object as its only parameter
+and returns a dictionary to add to the context.
+
+These are referenced from the setting TEMPLATE_CONTEXT_PROCESSORS and used by
+RequestContext.
+"""
+
+from mliitm import settings
+
+def default(request):
+    values = {
+                'SITE_URL':settings.SITE_URL,
+                'MEDIA_URL':settings.MEDIA_URL,
+            }
+
+    return values
+
+
---
+++ home/urls.py
@@ -0,0 +1,5 @@
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('mliitm.home.views',
+    (r'', 'home'),
+)
